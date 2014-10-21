74f93e3087ee95687804ec8acb40e4ef615ed99f
LI-JIANSHU <jianshu_li@sutd.edu.sg>
2014-10-21 17:42:59 +0800

    inital values for bias

--- generate_initialization.sh
+++ generate_initialization.sh
@@ -1,19 +1,29 @@
-dir=~/spn_initial_lala
+dir=~/spn_initial
 [ -d $dir ] || mkdir $dir
 # for the initial csv files
-echo 'Getting data from cnn...'
-cat exp_pdnn_110h/cnn/conv.nnet | grep "W 0" | sort -k3 -n | sed -e 's,^.*:,,' -e 's:"::g' -e 's:,::' -e 's:n: :g' -e 's:\\: :g' -e 's:^ ::' > $dir/input_conv1.data
-cat exp_pdnn_110h/cnn/conv.nnet | grep "W 1" | sort -k3 -k4 -n | sed -e 's,^.*:,,' -e 's:"::g' -e 's:,::' -e 's:n: :g' -e 's:\\: :g' -e 's:^ ::' > $dir/conv1_conv2.data
-cat exp_pdnn_110h/cnn/dnn.nnet | head -8402 | tail -4284 > $dir/h4_output.data
-cat exp_pdnn_110h/cnn/dnn.nnet | head -4113 | tail -1024 > $dir/h3_h4.data
-cat exp_pdnn_110h/cnn/dnn.nnet | head -3084 | tail -1024 > $dir/h2_h3.data
-cat exp_pdnn_110h/cnn/dnn.nnet | head -2055 | tail -1024 > $dir/h1_h2.data
-cat exp_pdnn_110h/cnn/dnn.nnet | head -1026 | tail -1024 > $dir/conv2_h1.data
+
+conv_nnet=exp_pdnn_110h/cnn/conv.nnet
+dnn_nnet=exp_pdnn_110h/cnn/dnn.nnet
+cat $conv_nnet | grep "W 0" | sort -k3 -n | sed -e 's,^.*:,,' -e 's:"::g' -e 's:,::' -e 's:n: :g' -e 's:\\: :g' -e 's:^ ::' > $dir/input_conv1.weight
+cat $conv_nnet | grep "W 1" | sort -k3 -k4 -n | sed -e 's,^.*:,,' -e 's:"::g' -e 's:,::' -e 's:n: :g' -e 's:\\: :g' -e 's:^ ::' > $dir/conv1_conv2.weight
+cat $dnn_nnet | head -8402 | tail -4284 > $dir/h4_output.weight
+cat $dnn_nnet | head -4113 | tail -1024 > $dir/h3_h4.weight
+cat $dnn_nnet | head -3084 | tail -1024 > $dir/h2_h3.weight
+cat $dnn_nnet | head -2055 | tail -1024 > $dir/h1_h2.weight
+cat $dnn_nnet | head -1026 | tail -1024 > $dir/conv2_h1.weight
+
+cat $conv_nnet | grep 'b 0' | sed -e 's,^.*: ,,' -e 's:"::g' -e 's:,::' -e 's:n: :g' -e 's:\\::g' > $dir/conv1.bias
+cat $conv_nnet | grep 'b 1' | sed -e 's,^.*: ,,' -e 's:"::g' -e 's:,::' -e 's:n: :g' -e 's:\\::g' > $dir/conv2.bias
+cat $dnn_nnet | head -8404 | tail -1 | sed -e 's:\[ ::' -e 's:\ ]::' > $dir/output.bias
+cat $dnn_nnet | head -4115 | tail -1 | sed -e 's:\[ ::' -e 's:\ ]::' > $dir/h4.bias
+cat $dnn_nnet | head -3086 | tail -1 | sed -e 's:\[ ::' -e 's:\ ]::' > $dir/h3.bias
+cat $dnn_nnet | head -2057 | tail -1 | sed -e 's:\[ ::' -e 's:\ ]::' > $dir/h2.bias
+cat $dnn_nnet | head -1028 | tail -1 | sed -e 's:\[ ::' -e 's:\ ]::' > $dir/h1.bias
 
 cp steps_deeplearn/convert.m $dir/
-echo "Going into $dir to convert data to csv format..."
 ( cd $dir;
-octave --silent convert.m ;
+octave --silent convert.m;
 rm convert.m
 )
+
 echo Finished at `date`
